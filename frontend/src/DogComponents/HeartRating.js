dcd6a327623a8027d2f42b98b13babdc43fb0a8d
Janzen Lim <janzen0821lim@gmail.com>
2020-05-21 18:58:54 +1000

    Cleaned up repository

---
+++ frontend/src/DogComponents/HeartRating.js
@@ -0,0 +1,51 @@
+import React, {useState, useContext} from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Rating from '@material-ui/lab/Rating';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import './HeartRating.css';
+import { DogContext } from './DogContext';
+
+var heartValue = 5;
+const StyledRating = withStyles({
+  iconFilled: {
+    color: '#ff3d47',
+  }
+})(Rating);
+
+function getLabelText(value) {
+  return `${value} Heart${value !== 1 ? 's' : ''}`;
+}
+
+function pHeartValue(){
+  heartValue++;
+}
+
+function mHeartValue(){
+  heartValue--;
+}
+export default function HeartRatings() {
+
+  const [heartState, setHeartState] = useState(5);
+  const {state, changeState, setStateHappy, setStateSad} = useContext(DogContext);
+
+  if (heartState < 5 && state === "happy"){
+    setHeartState(prev => prev + 1);
+  } else if (heartState > 0 && state === "sad"){
+    setHeartState(prev => prev - 1);
+  }
+
+  return (
+    <div className = 'heart'>
+        <StyledRating
+          name="customized-color"
+          value={heartState}
+          getLabelText={getLabelText}
+          precision={0.5}
+          icon={<FavoriteIcon fontSize="inherit" />}
+          size='large'
+        />
+    </div>
+  );
+}
